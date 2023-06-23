f0fd01521bef8878f1a264cb386c4c268883198f
cafasaru <maildonvader@gmail.com>
2023-06-23 14:59:35 -0600

    added Health and Ping methods for the Health service

--- internal/health/health.go
+++ internal/health/health.go
@@ -1,14 +1,18 @@
-// Package health is responsible for providing the status of our microservice health
-// also is used for k8s status
+// Package health is responsible for providing the status of our microservice
 package health
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-// Health is a representation of a health struct which will provide db status
+var (
+	ErrPingDB = errors.New("failed to ping the database")
+)
+
+// Health is a representation of the health struct which will provide service status
 type Health struct {
 	Name    string `json:"service"`
 	Version string `json:"version"`
@@ -16,7 +20,7 @@ type Health struct {
 	Message string `json:"message"`
 }
 
-// Store represents the interface with which we interact with the repository layer
+// Store represents the interface used to interact with the repository
 type Store interface {
 	// PingDB pings the database
 	PingDB(ctx context.Context) error
@@ -52,7 +56,7 @@ func (s *Service) Ping(ctx context.Context) (Health, error) {
 // Health pings the database and runs a simple select statment to verify the database is ready for query exec.
 func (s *Service) Health(ctx context.Context) (Health, error) {
 	// Call the repository ping to ping the database
-	fmt.Println("call the database for status")
+	fmt.Println("pinging database")
 
 	if err := s.Store.PingDB(ctx); err != nil {
 		fmt.Println(err)
@@ -61,7 +65,7 @@ func (s *Service) Health(ctx context.Context) (Health, error) {
 			Version: s.Version,
 			Status:  http.StatusNotAcceptable,
 			Message: "failed to ping db",
-		}, nil
+		}, ErrPingDB
 	}
 
 	return Health{
